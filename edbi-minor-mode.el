a93d3255e0bf9b11ed0aa76786742c3454c04229
Malyshev Artem <proofit404@gmail.com>
2015-01-28 16:13:46 +0300

    Write file header.

--- edbi-minor-mode.el
+++ edbi-minor-mode.el
@@ -1,3 +1,31 @@
+;;; edbi-minor-mode.el --- Use edbi with regular SQL files.
+
+;; Copyright (C) 2015 by Artem Malyshev
+
+;; Author: Artem Malyshev <proofit404@gmail.com>
+;; URL: https://github.com/proofit404/edbi-minor-mode
+;; Version: 0.0.1
+;; Package-Requires: ((edbi "0.1.3"))
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; See the README for more details.
+
+;;; Code:
+
 (require 'edbi)
 
 (defvar-local edbi-minor-mode-connection nil "")
@@ -56,3 +84,7 @@
   (interactive)
   (let ((sql (buffer-substring-no-properties (point-min) (point-max))))
     (edbi-minor-mode-execute sql)))
+
+(provide 'edbi-minor-mode)
+
+;;; edbi-minor-mode.el ends here
