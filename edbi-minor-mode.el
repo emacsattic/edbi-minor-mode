3fc439598b9a59e2b73ff52abe24d76c98ee071b
Malyshev Artem <proofit404@gmail.com>
2015-01-28 10:21:42 +0300

    Implement minor mode.

--- edbi-minor-mode.el
+++ edbi-minor-mode.el
@@ -1,12 +1,58 @@
+(require 'edbi)
+
 (defvar-local edbi-minor-mode-connection nil "")
 
 (defvar-local edbi-minor-mode-result-buffer nil "")
 
-(setq edbi-minor-mode-connection (buffer-local-value 'edbi:connection (get-buffer "*edbi-dbviewer*")))
+(defvar edbi-minor-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map [remap sql-send-buffer] 'edbi-minor-mode-execute-buffer)
+    (define-key map [remap sql-send-paragraph] 'edbi-minor-mode-execute-paragraph)
+    (define-key map [remap sql-send-region] 'edbi-minor-mode-execute-region)
+    (define-key map [remap sql-send-string] 'edbi-minor-mode-execute)
+    map)
+  "")
 
-(setq edbi-minor-mode-result-buffer (get-buffer-create "*edbi-minor-mode-result*"))
+(define-minor-mode edbi-minor-mode
+  ""
+  :lighter " Edbi")
 
 (defun edbi-minor-mode-execute (sql)
-  (edbi:dbview-query-execute edbi-minor-mode-connection sql edbi-minor-mode-result-buffer))
+  ""
+  (interactive "sSQL: ")
+  (unless edbi-minor-mode-connection
+    (setq edbi-minor-mode-connection
+          (buffer-local-value 'edbi:connection
+                              (get-buffer edbi:dbview-buffer-name))))
+  (unless edbi-minor-mode-result-buffer
+    (setq edbi-minor-mode-result-buffer
+          (get-buffer-create
+           (generate-new-buffer-name "*edbi-minor-mode-result*"))))
+  (edbi:dbview-query-execute
+   edbi-minor-mode-connection
+   sql
+   edbi-minor-mode-result-buffer))
 
-(edbi-minor-mode-execute (buffer-substring-no-properties (point-min) (point-max)))
+(defun edbi-minor-mode-execute-paragraph ()
+  ""
+  (interactive)
+  (let* ((start (save-excursion
+                  (backward-paragraph)
+                  (point)))
+         (end (save-excursion
+                (forward-paragraph)
+                (point)))
+         (sql (buffer-substring-no-properties start end)))
+    (edbi-minor-mode-execute sql)))
+
+(defun edbi-minor-mode-execute-region (start end)
+  ""
+  (interactive "r")
+  (let ((sql (buffer-substring-no-properties start end)))
+    (edbi-minor-mode-execute sql)))
+
+(defun edbi-minor-mode-execute-buffer ()
+  ""
+  (interactive)
+  (let ((sql (buffer-substring-no-properties (point-min) (point-max))))
+    (edbi-minor-mode-execute sql)))
